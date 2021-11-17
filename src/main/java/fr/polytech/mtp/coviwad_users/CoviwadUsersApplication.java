35551fabde4130051e8bfe61a47485f4501578eb
Solene-Issartel <issartel.solene@gmail.com>
2021-11-17 16:10:54 +0100

    Initial commit

---
+++ src/main/java/fr/polytech/mtp/coviwad_users/CoviwadUsersApplication.java
@@ -0,0 +1,13 @@
+package fr.polytech.mtp.coviwad_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoviwadUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoviwadUsersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/polytech/mtp/coviwad_users/CoviwadUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.polytech.mtp.coviwad_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoviwadUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
