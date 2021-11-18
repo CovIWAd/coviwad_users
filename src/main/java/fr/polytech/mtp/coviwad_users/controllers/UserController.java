e1be9bfe098d29e224d2c55eed158c024d0b0884
Marine Téroitin <55442169+marineteroitin@users.noreply.github.com>
2021-11-18 17:44:57 +0100

    [ADD] check attributs not null

--- src/main/java/fr/polytech/mtp/coviwad_users/controllers/UserController.java
+++ src/main/java/fr/polytech/mtp/coviwad_users/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import javax.annotation.security.RolesAllowed;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
@@ -33,9 +34,19 @@ public class UserController {
     public User update (@PathVariable String id, @RequestBody User user){
 
         if(id != null && user != null && userRepository.findById(id).isPresent()){
-            User existingUser = userRepository.getById(id);
-            BeanUtils.copyProperties(user, existingUser, "id");
-            return userRepository.saveAndFlush(existingUser);
+            if((user.getFirst_name() != null)
+                    && (user.getFirst_name().equals(""))
+                    && (user.getLast_name() != null)
+                    && (user.getLast_name().equals(""))
+            ){
+                User existingUser = userRepository.getById(id);
+                BeanUtils.copyProperties(user, existingUser, "id");
+                return userRepository.saveAndFlush(existingUser);
+            }else{
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Attributes cannot be null");
+
+            }
+
         } else {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Missing arguments  or User with ID "+id+" not found");
 
